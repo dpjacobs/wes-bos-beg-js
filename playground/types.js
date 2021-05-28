b94ed83de7217359d29b132f6111befab2de219b
dpjacobs <dpjacobs@gmail.com>
2021-05-27 22:26:18 -0700

    052721

---
+++ playground/types.js
@@ -0,0 +1,13 @@
+const first = 'wes';
+const middle = 'topher';
+const last = `bos`;
+const sentence = "she's so cool";
+
+const song = `
+oh
+yea
+i
+like
+pizza`;
+const name = `David`
+const hello = `Hello my name is ${name}. Nice to meet you.`
